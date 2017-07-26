f6d5385b8f006e47a45802ed127eb4a07230f967
dannytce <dankijkov@gmail.com>
2017-07-26 17:48:06 +0200

    production minification of javascript

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [

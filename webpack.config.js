81be0d6e8fb2ea530cb9ec719fccf60b743b9919
dannytce <dankijkov@gmail.com>
2017-07-26 18:24:27 +0200

    html minifikace

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,18 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(), // Enable HMR
     new HtmlWebpackPlugin({
       template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
     }),
     new ExtractTextPlugin('style.[hash].css'),
   ],

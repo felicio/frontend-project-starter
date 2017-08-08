b16e85f353b5a11390b5617ec3cc8939b7ae5412
dannytce <dankijkov@gmail.com>
2017-08-08 18:36:53 +0200

    03/assets (#1)
    * add file and url loader
    
    * responsive video
    
    * production minification of javascript
    
    * minifying css
    
    * html minifikace
    
    * dev vs prod
    
    * moved dist folder to top level
    
    * add webpack/config.base
    
    * rimraf instead of clean-webpack-plugin
    
    * cleanup

---
+++ internals/webpack/config.prod.js
@@ -0,0 +1,24 @@
+/* eslint-env node */
+/* eslint-disable no-inline-comments */
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = require('./config.base')({
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    }),
+  ],
+})

9cb63fbdb603799d115cdd97b5faf55121b9aa24
dannytce <dankijkov@gmail.com>
2017-08-09 17:54:01 +0200

    components examples

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
 
+const Header = () => <h1>Frontend Starter Project - React</h1>
+
+function Main() {
+  return (
+    <main>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam veritatis ipsa vel aut
+        corporis minus commodi temporibus voluptatem ipsum dolore?
+      </p>
+    </main>
+  )
+}
+
+class Footer extends Component {
+  render() {
+    return <footer>Copyright &copy; STRV Academy 2017</footer>
+  }
+}
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Frontend Starter Project - React</h1>
+        <Header />
+        <Main />
+        <Footer />
       </div>
     )
   }

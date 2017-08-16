5f0994320285adfcb0825d4e697be17b470615f7
dannytce <dankijkov@gmail.com>
2017-08-16 19:01:40 +0200

    html

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import App from './components/App'
 
 import './styles/style.scss'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// ReactDOM.render(<App />, document.getElementById('root'))

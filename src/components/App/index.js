6cb5ab99879226c125413a697b4f0c1f0aec4de4
dannytce <dankijkov@gmail.com>
2017-08-16 17:13:09 +0200

    add router example

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Route, Link, BrowserRouter } from 'react-router-dom'
+
+import Home from '../Home'
+import About from '../About'
+import Topics from '../Topics'
 
 const App = () =>
-  <div>
-    <h1>Frontend Starter Project - React</h1>
-  </div>
+  <BrowserRouter>
+    <div>
+      <h1>Frontend Starter Project - React</h1>
+
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </div>
+    </div>
+  </BrowserRouter>
 
 export default App

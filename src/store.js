f4815cbe7a2b1708a69142208c4c99cd7140fc67
dannytce <dankijkov@gmail.com>
2017-08-23 18:02:13 +0200

    fix eslint warning in store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { createStore } from 'redux'
 import reducer from './data/todo/reducer'
 
-
 export default createStore(
   reducer,
+  // eslint-disable-next-line no-underscore-dangle
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
